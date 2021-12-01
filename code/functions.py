421d7f64451763f85142cf7d284d1c5c807f3a07
kurasoo6 <verneuilpaul@gmail.com>
2021-12-01 23:20:53 +0100

    scrapping page produit et écriture des donnée dans csv; ajout fichier functions.py

---
+++ code/functions.py
@@ -0,0 +1,5 @@
+def rating_in_letter_to_interger(rating_in_letter):
+    numbers={"Zero":0, "One":1, "Two":2, "Three":3, "Four":4, "Five":5} 
+    rating_int=numbers[rating_in_letter]
+    return rating_int
+
