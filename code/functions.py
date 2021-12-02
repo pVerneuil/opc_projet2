4400de52f7be7fbb86a10dc8d3f8ecdc13aac39c
kurasoo6 <verneuilpaul@gmail.com>
2021-12-03 00:38:21 +0100

    trasformation en fonction du script de récuperation des données sur un page produit + récupération des catégories

--- code/functions.py
+++ code/functions.py
@@ -1,5 +1,61 @@
+import requests;
+from bs4 import BeautifulSoup
+import re
+import csv
+
+#comments go there
 def rating_in_letter_to_interger(rating_in_letter):
     numbers={"Zero":0, "One":1, "Two":2, "Three":3, "Four":4, "Five":5} 
     rating_int=numbers[rating_in_letter]
     return rating_int
 
+#comments go there
+def get_data_from_product_page(product_page_url,category):
+    page=requests.get(product_page_url)
+    soup=BeautifulSoup(page.content,'html.parser')
+    if page.ok:
+        
+        table=soup.find('table',"table table-striped")
+        rows = table.find_all('tr')
+        data = []
+        for row in rows:
+            cols = row.find_all('td')
+            for item in cols:
+                item=item.text
+                data.append(item)
+
+        upc=data[0]
+        price_including_tax=data[2]
+        price_excluding_tax=data[3]
+        extracted_number = re.findall(r'\d+', data[5])
+        number_available=extracted_number[0]
+        #title
+        title=soup.find('li',class_='active').text
+        #description
+        on_top_of_description=soup.find('div', class_="sub-header")
+        description=on_top_of_description.find_next('p').text
+        #image_url
+        image=soup.find('div',class_="item active").img
+        image_path=image['src']
+        image_url='http://books.toscrape.com'+image_path[5:] #supression des 5 premiers characteres
+        #review_rating
+        review_rating_tag=soup.find('p','star-rating').attrs #<-- revoie un dictionnaire ou la note est l'indice 1 dans la liste associer a la clé class
+        review_rating_letter=review_rating_tag['class'][1]
+        review_rating=rating_in_letter_to_interger(review_rating_letter)
+        product_data=[product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available,description, category,review_rating, image_url]
+        return product_data
+
+
+def get_all_categories():
+    product_page_url='http://books.toscrape.com/index.html'
+    page=requests.get(product_page_url)
+    soup=BeautifulSoup(page.content,'html.parser')
+    test=soup.find_all('a',href=re.compile("category"))
+    categories=[]
+    for links in test:
+        full_path=links['href']
+        temp0=full_path[25:]
+        temp=temp0[:-11]
+        categories.append(temp)
+    del categories[0]
+    return categories
--- code/main.py
+++ code/main.py
@@ -4,48 +4,9 @@ from bs4 import BeautifulSoup
 import re
 import csv
 
-product_page_url='http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-page=requests.get(product_page_url)
-soup=BeautifulSoup(page.content,'html.parser')
-if page.ok:
+categories=get_all_categories()
 
-    #upc, price_including_tax, price_excluding_tax, number_available
-    table=soup.find('table',"table table-striped")
-    rows = table.find_all('tr')
-    data = []
-    for row in rows:
-        cols = row.find_all('td')
-        for item in cols:
-            item=item.text
-            data.append(item)
-
-    upc=data[0]
-    price_including_tax=data[2]
-    price_excluding_tax=data[3]
-    extracted_number = re.findall(r'\d+', data[5])
-    number_available=extracted_number[0]
-    #title
-    title=soup.find('li',class_='active').text
-    #description
-    on_top_of_description=soup.find('div', class_="sub-header")
-    description=on_top_of_description.find_next('p').text
-    #image_url
-    image=soup.find('div',class_="item active").img
-    image_path=image['src']
-    image_url='http://books.toscrape.com'+image_path[5:] #supression des 5 premiers characteres
-    #review_rating
-    review_rating_tag=soup.find('p','star-rating').attrs #<-- revoie un dictionnaire ou la note est l'indice 1 dans la liste associer a la clé class
-    review_rating_letter=review_rating_tag['class'][1]
-    review_rating=rating_in_letter_to_interger(review_rating_letter)
-    
-    #csv
-    header=['product_page_url', 'universal_product_code', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available','product_description', 'category', 'review_rating', 'image_url']
-    product_data=[product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available,description, 'category_do_latter',review_rating, image_url]
-    with open('csv/data.csv','w') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        writer.writerow(header)
-        writer.writerow(product_data)
-        
-
- 
+a=[1,2,3,4,5]
+for i in a:
+    print(a[i-1])
  
