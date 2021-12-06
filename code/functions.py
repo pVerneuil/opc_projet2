ae651c1771fe57cb2afcc5d0b3856cad852d4e8c
kurasoo6 <verneuilpaul@gmail.com>
2021-12-06 22:22:38 +0100

    feat: added a function to scrapp all the url of catalog  pages for a category (pagination), and an another one to get all the product page's urls from a caralog page. Main.py now get all the data of the website

--- code/functions.py
+++ code/functions.py
@@ -1,61 +1,111 @@
-import requests;
-from bs4 import BeautifulSoup
 import re
 import csv
 
-#comments go there
+import requests
+from bs4 import BeautifulSoup
+
+
+# Transform rating in srting to rating in interger
 def rating_in_letter_to_interger(rating_in_letter):
-    numbers={"Zero":0, "One":1, "Two":2, "Three":3, "Four":4, "Five":5} 
-    rating_int=numbers[rating_in_letter]
+    numbers = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+    rating_int = numbers[rating_in_letter]
     return rating_int
 
-#comments go there
-def get_data_from_product_page(product_page_url,category):
-    page=requests.get(product_page_url)
-    soup=BeautifulSoup(page.content,'html.parser')
+
+# Scrap thoose data from a product page:
+#  product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available, description, category, review_rating, image_url
+def get_data_from_product_page(product_page_url, category):
+    page = requests.get(product_page_url)
+    soup = BeautifulSoup(page.content, "html.parser")
     if page.ok:
-        
-        table=soup.find('table',"table table-striped")
-        rows = table.find_all('tr')
+
+        table = soup.find("table", "table table-striped")
+        rows = table.find_all("tr")
         data = []
         for row in rows:
-            cols = row.find_all('td')
+            cols = row.find_all("td")
             for item in cols:
-                item=item.text
+                item = item.text
                 data.append(item)
 
-        upc=data[0]
-        price_including_tax=data[2]
-        price_excluding_tax=data[3]
-        extracted_number = re.findall(r'\d+', data[5])
-        number_available=extracted_number[0]
-        #title
-        title=soup.find('li',class_='active').text
-        #description
-        on_top_of_description=soup.find('div', class_="sub-header")
-        description=on_top_of_description.find_next('p').text
-        #image_url
-        image=soup.find('div',class_="item active").img
-        image_path=image['src']
-        image_url='http://books.toscrape.com'+image_path[5:] #supression des 5 premiers characteres
-        #review_rating
-        review_rating_tag=soup.find('p','star-rating').attrs #<-- revoie un dictionnaire ou la note est l'indice 1 dans la liste associer a la clé class
-        review_rating_letter=review_rating_tag['class'][1]
-        review_rating=rating_in_letter_to_interger(review_rating_letter)
-        product_data=[product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available,description, category,review_rating, image_url]
+        upc = data[0]
+        price_including_tax = data[2]
+        price_excluding_tax = data[3]
+        extracted_number = re.findall(r"\d+", data[5])
+        number_available = extracted_number[0]
+        title = soup.find("li", class_="active").text
+        on_top_of_description = soup.find("div", class_="sub-header")
+        description = on_top_of_description.find_next("p").text
+        image = soup.find("div", class_="item active").img
+        image_path = image["src"]
+        image_url = "http://books.toscrape.com" + image_path[5:]
+        review_rating_tag = soup.find("p", "star-rating").attrs
+        review_rating_letter = review_rating_tag["class"][1]
+        review_rating = rating_in_letter_to_interger(review_rating_letter)
+        product_data = [
+            product_page_url,
+            upc,
+            title,
+            price_including_tax,
+            price_excluding_tax,
+            number_available,
+            description,
+            category,
+            review_rating,
+            image_url,
+        ]
         return product_data
 
 
-def get_all_categories():
-    product_page_url='http://books.toscrape.com/index.html'
-    page=requests.get(product_page_url)
-    soup=BeautifulSoup(page.content,'html.parser')
-    test=soup.find_all('a',href=re.compile("category"))
-    categories=[]
-    for links in test:
-        full_path=links['href']
-        temp0=full_path[25:]
-        temp=temp0[:-11]
-        categories.append(temp)
+# Scrap and return all category from the index
+def get_all_categories(index):
+    page = requests.get(index)
+    soup = BeautifulSoup(page.content, "html.parser")
+    links_containing_category = soup.find_all("a", href=re.compile("category"))
+    categories = []
+    for links in links_containing_category:
+        full_path = links["href"]
+        only_category = full_path[25:][:-11]
+        categories.append(only_category)
     del categories[0]
     return categories
+
+
+# Return all catalogue page's urls for a give category
+def get_catalogue_urls_from_a_category(category):
+    urls = []
+    url_index = (
+        "http://books.toscrape.com/catalogue/category/books/" + category + "/index.html"
+    )
+    urls.append(url_index)
+    i = 2
+    while True:
+        url_pages = (
+            "http://books.toscrape.com/catalogue/category/books/"
+            + category
+            + "/page-"
+            + str(i)
+            + ".html"
+        )
+        page = requests.get(url_pages)
+        if page.ok:
+            urls.append(url_pages)
+            i += 1
+        else:
+            break
+    return urls
+
+
+# Scraps urls of product pages from a catalogue page
+
+
+def get_page_product_url(catalogue_url):
+    page = requests.get(catalogue_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    h3 = soup.find_all("h3")
+    product_page_urls = []
+    for titre in h3:
+        path = titre.a["href"][8:]
+        url = "https://books.toscrape.com/catalogue" + path
+        product_page_urls.append(url)
+    return product_page_urls
--- code/main.py
+++ code/main.py
@@ -1,12 +1,23 @@
 from functions import *
-import requests;
-from bs4 import BeautifulSoup
-import re
-import csv
+import time
+# *todo: download image and save it in file
 
-categories=get_all_categories()
+start_time = time.time()
 
-a=[1,2,3,4,5]
-for i in a:
-    print(a[i-1])
- 
+page_to_scrap = "https://books.toscrape.com/"
+data = []
+for category in get_all_categories(page_to_scrap):
+    i = 0
+    y = 1
+    page_catalogue = get_catalogue_urls_from_a_category(category)
+    for page in page_catalogue:
+        temp = []
+        i += 1
+        temp.extend(get_page_product_url(page))
+        for item in temp:
+            # print(category+' page '+str(i)+' livre '+ str(y) +':')
+            y += 1
+            # print(get_data_from_product_page(item,category))
+            data.append(get_data_from_product_page(item, category))
+print(data)
+print("--- %s seconds ---" % (time.time() - start_time))
